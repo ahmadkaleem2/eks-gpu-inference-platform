c1f5956c84fe7142d554a7f07da9a25a241ba42f
ahmadkaleem2 <ahmad.kaleem2.123@gmail.com>
2026-09-24 19:28:49 +0500

    fmt

--- Infra/networking/locals.tf
+++ Infra/networking/locals.tf
@@ -1,4 +1,4 @@
 locals {
   folder_array = split("/", abspath("."))
-  created_by = join("/", ["https://github.com/ahmadkaleem2/EKS-GPU-Inference-Platform"], slice(local.folder_array, index(local.folder_array, "Infra"), length(local.folder_array)))
+  created_by   = join("/", ["https://github.com/ahmadkaleem2/EKS-GPU-Inference-Platform"], slice(local.folder_array, index(local.folder_array, "Infra"), length(local.folder_array)))
 }
